e08f3705b61d5a0e3f48abe72f5566167c70c955
hugo <vvhooping@gmail.com>
2024-09-27 18:58:27 +0900

    Day 14 : 한입-씨네마 스트리밍 적용하기 (#6)

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -4,6 +4,15 @@ import styles from './page.module.css';
 
 import { MovieData } from '@/types';
 
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정 (부작용: 검색 기능 등이 제대로 동작하지 않는다.)
+// 4. error : 페이지를 강제로 Static 페이지로 설정 (Static 으로 설정할 수 없는 이유가 있다면 빌드 오류를 발생시킴)
+
+// dynamic 은 부작용이 발생할 위험이 있으므로 별로 사용을 권장하는 옵션은 아니지만, 알고는 있어야 함.
+// export const dynamic = 'auto';
+
 async function AllMovies() {
   // 전체 영화 목록은 현재 데이터가 변경될 일이 없으므로 force-cache 를 적용한다.
   const response = await fetch(
