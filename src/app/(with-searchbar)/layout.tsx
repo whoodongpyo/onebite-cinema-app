9bafe92435c58be31c41e90cb4720f753edd6aaf
hugo <vvhooping@gmail.com>
2024-09-27 17:56:10 +0900

    feat: 풀 라우트 캐시 적용하기

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,11 +1,14 @@
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 
 import Searchbar from '@/components/searchbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Searchbar />
+      <Suspense fallback={<div>로딩중입니다...</div>}>
+        <Searchbar />
+      </Suspense>
+
       {children}
     </div>
   );
