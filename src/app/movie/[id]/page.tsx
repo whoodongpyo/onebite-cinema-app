0e9146e5a1ff86c7f0236de06b79cf3ded7d7d5f
hugo <vvhooping@gmail.com>
2024-09-27 13:14:54 +0900

    style: 영화 상페 페이지 스타일링

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,7 +1,56 @@
+import { MovieData } from '@/types';
+
+import styles from './page.module.css';
+import movies from '@/mock/dummy.json';
+
 export default function Page({
   params,
 }: {
   params: { id: string | string[] };
 }) {
-  return <div>movie/[id] 페이지 {params.id}</div>;
+  const { id } = params;
+
+  const foundMovie: MovieData | undefined = movies.find(
+    (movie) => movie.id === Number(id),
+  );
+
+  if (!foundMovie) {
+    return '존재하지 않는 영화 정보입니다. 다시 시도해주세요.';
+  }
+
+  const {
+    // id,
+    title,
+    releaseDate,
+    company,
+    genres,
+    subTitle,
+    description,
+    runtime,
+    posterImgUrl,
+  } = foundMovie;
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.cover_img_container}
+        style={{ backgroundImage: `url('${posterImgUrl}')` }}
+      >
+        <img src={posterImgUrl} alt={title} />
+      </div>
+      <div className={styles.info_container}>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            {releaseDate} / {genres.join(', ')} / {runtime}분
+          </div>
+          <div>{company}</div>
+        </div>
+        <div>
+          <div className={styles.subTitle}>{subTitle}</div>
+          <div className={styles.description}>{description}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
