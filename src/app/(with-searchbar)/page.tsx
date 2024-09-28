139e815618b7e9fe239be034a8aee3a5ed1e8d3b
hugo <vvhooping@gmail.com>
2024-09-28 14:38:15 +0900

    Day 15 : 한입-씨네마 컴포넌트별 스트리밍 적용하기 (with Suspense) (#7)
    * feat: 검색 페이지에 스트리밍 적용
    
    * feat: Index 페이지에 스트리밍 적용
    
    * feat: Index 페이지 스켈레톤 코드 적용
    
    * feat: 검색 페이지 스켈레톤 코드 적용
    
    * feat: 에러 핸들링 실습

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,6 +3,9 @@ import MovieItem from '@/components/movie-item';
 import styles from './page.module.css';
 
 import { MovieData } from '@/types';
+import delay from '@/util/delay';
+import { Suspense } from 'react';
+import MovieListSkeleton from '@/components/skeleton/movie-list-skeleton';
 
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
 // 1. auto : 기본값, 아무것도 강제하지 않음
@@ -14,6 +17,8 @@ import { MovieData } from '@/types';
 // export const dynamic = 'auto';
 
 async function AllMovies() {
+  await delay(1500);
+
   // 전체 영화 목록은 현재 데이터가 변경될 일이 없으므로 force-cache 를 적용한다.
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
@@ -26,16 +31,14 @@ async function AllMovies() {
 
   const allMovies: MovieData[] = await response.json();
 
-  return (
-    <div className={styles.all_container}>
-      {allMovies.map((movie) => (
-        <MovieItem key={`all-${movie.id}`} {...movie} />
-      ))}
-    </div>
-  );
+  return allMovies.map((movie) => (
+    <MovieItem key={`all-${movie.id}`} {...movie} />
+  ));
 }
 
 async function RecoMovies() {
+  await delay(3000);
+
   // 추천 목록은 계속 무작위로 보여줘야 하기 때문에
   // 특정 시간을 주기로 업데이트하도록 revalidate 로 적용한다.
   const response = await fetch(
@@ -49,25 +52,31 @@ async function RecoMovies() {
 
   const recoMovies: MovieData[] = await response.json();
 
-  return (
-    <div className={styles.reco_container}>
-      {recoMovies.map((movie) => (
-        <MovieItem key={`reco-${movie.id}`} {...movie} />
-      ))}
-    </div>
-  );
+  return recoMovies.map((movie) => (
+    <MovieItem key={`reco-${movie.id}`} {...movie} />
+  ));
 }
 
-export default async function Home() {
+export const dynamic = 'force-dynamic';
+
+export default function Home() {
   return (
     <div className={styles.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <RecoMovies />
+        <div className={styles.reco_container}>
+          <Suspense fallback={<MovieListSkeleton count={3} />}>
+            <RecoMovies />
+          </Suspense>
+        </div>
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <AllMovies />
+        <div className={styles.all_container}>
+          <Suspense fallback={<MovieListSkeleton count={20} />}>
+            <AllMovies />
+          </Suspense>
+        </div>
       </section>
     </div>
   );
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,18 +1,14 @@
+import { Suspense } from 'react';
 import MovieItem from '@/components/movie-item';
 
 import styles from './page.module.css';
 
 import { MovieData } from '@/types';
 import delay from '@/util/delay';
+import MovieListSkeleton from '@/components/skeleton/movie-list-skeleton';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { q?: string };
-}) {
-  await delay(1500);
-
-  const { q } = searchParams;
+async function SearchResult({ q }: { q: string }) {
+  await delay(1000);
 
   // 현재는 영화 데이터가 변경될 일이 없으므로, force-cache 를 적용한다.
   const response = await fetch(
@@ -26,11 +22,21 @@ export default async function Page({
 
   const searchedMovies: MovieData[] = await response.json();
 
+  return searchedMovies.map((movie) => <MovieItem key={movie.id} {...movie} />);
+}
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { q?: string };
+}) {
   return (
     <div className={styles.search_container}>
-      {searchedMovies.map((movie) => (
-        <MovieItem key={movie.id} {...movie} />
-      ))}
+      <Suspense
+        key={searchParams.q || ''}
+        fallback={<MovieListSkeleton count={12} />}
+      >
+        <SearchResult q={searchParams.q || ''} />
+      </Suspense>
     </div>
   );
 }
