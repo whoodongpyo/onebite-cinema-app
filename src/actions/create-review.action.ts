c93e154b94af07b09d039f701cda06c57f689b8a
hugo <vvhooping@gmail.com>
2024-09-29 21:30:26 +0900

    feat: 클라이언트 컴포넌트에서 서버 액션 실습

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,41 +1,48 @@
 'use server'; // 서버 액션 설정
 
 import { revalidatePath, revalidateTag } from 'next/cache';
+import delay from '@/util/delay';
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(
+  previousState: any,
+  formData: FormData,
+) {
   const movieId = formData.get('movieId')?.toString();
   const content = formData.get('content')?.toString();
   const author = formData.get('author')?.toString();
 
-  if (!movieId || !content || !author) return;
+  await delay(2000);
+
+  if (!movieId || !content || !author) {
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해 주세요.',
+    };
+  }
 
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/1`,
       {
         method: 'POST',
         body: JSON.stringify({ movieId, content, author }),
       },
     );
 
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
-    // 1. 특정 주소에 해당하는 페이지만 재검증
-    // revalidatePath(`/movie/${movieId}`);
-
-    // 2. 특정 경로의 모든 동적 페이지들을 재검증
-    // revalidatePath('/movie/[id]', 'page');
-
-    // 3. 특정 레이아웃을 갖는 모든 페이지들을 재검증
-    // revalidatePath('/(with-searchbar)', 'layout');
-
-    // 4. 모든 데이터 재검증
-    // revalidatePath('/', 'layout');
-
-    // 5. 태그를 기준으로 데이터 캐시 재검증
     revalidateTag(`review-${movieId}`);
+
+    return {
+      status: true,
+      error: '',
+    };
   } catch (error) {
-    console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다. : ${error}`,
+    };
   }
 }
--- src/components/review-editor.tsx
+++ src/components/review-editor.tsx
@@ -1,16 +1,44 @@
+'use client';
+
+import { useActionState, useEffect } from 'react';
 import { createReviewAction } from '@/actions/create-review.action';
 
 import styles from './review-editor.module.css';
 
 export default function ReviewEditor({ movieId }: { movieId: string }) {
+  // state : Action 이 return 한 결과가 담긴다.
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null,
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className={styles.form_container} action={createReviewAction}>
+      <form className={styles.form_container} action={formAction}>
         <input type="hidden" name="movieId" value={movieId} />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={styles.submit_container}>
-          <input required type="text" name="author" placeholder="작성자" />
-          <button type="submit">작성하기</button>
+          <input
+            required
+            disabled={isPending}
+            type="text"
+            name="author"
+            placeholder="작성자"
+          />
+          <button disabled={isPending} type="submit">
+            {isPending ? '...' : '작성하기'}
+          </button>
         </div>
       </form>
     </section>
